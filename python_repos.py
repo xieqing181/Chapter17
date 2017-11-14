634aa6fa6ac1f78a89eccab4e68816a62681b994
xieqing181 <33027473+xieqing181@users.noreply.github.com>
2017-11-14 16:58:34 +0800

    modified code for adding description and url

--- python_repos.py
+++ python_repos.py
@@ -1,10 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import requests
 import pygal
 from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
+import random
 
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 
 r = requests.get(url, timeout = 150)
+r.encoding ='utf-8'
 print("Status code: ", r.status_code)
 
 response_dict = r.json()
@@ -16,11 +21,20 @@ print("Total repositories: ", response_dict['total_count'])
 repo_dicts = response_dict['items']
 print("Repositories returned: ", len(repo_dicts))
 
-names, stars = [], []
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+    #if repo_dict['id'] == 1362490:	
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': repo_dict['description'],
+        'xlink': repo_dict['html_url'],
+        }
+    plot_dicts.append(plot_dict)
     
+print(plot_dicts)
+print('\n')
+#print(isinstance(plot_dicts,unicode))    
 my_style = LS("#333366", base_style=LCS)
 
 my_config = pygal.Config()
@@ -37,8 +51,8 @@ chart = pygal.Bar(my_config, style=my_style)
 chart.title = "Most-Starred Python Projects on GitHub"
 chart.x_labels = names
 
-chart.add('', stars)
-chart.render_to_file('python_repos.svg')
+chart.add('', plot_dicts)
+chart.render_to_file('python_repos_1.svg')
 
 '''
 #research the first Repository
